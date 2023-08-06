6790ee14f8d05fbac4498ea3ec1c77b39c3b9513
stylish_code <minyoung0101q@gmail.com>
2023-08-06 18:48:49 +0900

    LV0 DAY03 0805 나머지 구하기

---
+++ src/com/example/programmers/lv0/Solution08.java
@@ -0,0 +1,17 @@
+package com.example.programmers.lv0;
+//day0805 LV0 나머지 구하기
+/*
+* 문제 설명 : 정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요
+*  */
+public class Solution08 {
+    public int solution(int num1, int num2) {
+        int answer = num1 % num2;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution08 sol = new Solution08();
+        int result = sol.solution(3, 2);
+        System.out.println(result);
+    }
+}
