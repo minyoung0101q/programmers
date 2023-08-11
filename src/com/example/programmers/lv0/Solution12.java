a51d622b70faed33684244cb4f9451dc0b13ee9a
stylish_code <minyoung0101q@gmail.com>
2023-08-12 00:56:07 +0900

    day0807 LV0 피자 나눠먹기 (1)

---
+++ src/com/example/programmers/lv0/Solution12.java
@@ -0,0 +1,17 @@
+package com.example.programmers.lv0;
+//day0807 LV0 피자 나눠먹기 (1)
+public class Solution12 {
+    public int solution(int n) {
+        int answer = 0;
+        if(n%7 == 0)
+            answer = n / 7;
+        else
+            answer = n / 7 + 1;
+        return answer;
+    }
+    public static void main(String[] args) {
+        Solution12 sol12 = new Solution12();
+        int result = sol12.solution(10);
+        System.out.println(result);
+    }
+}
